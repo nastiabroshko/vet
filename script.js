2ef80fe0e20e5538d07d2f98a62686c09dcbaaef
nastiabroshko <nastiabroshko05@gmail.com>
2023-04-25 12:16:07 +0300

    add full site

---
+++ script.js
@@ -0,0 +1,8 @@
+function validate(){
+    let password = document.getElementById("password_user").value;
+        // alert( password.length)
+        if(password.length<8){
+            
+        }
+    
+}
